7470339488ada46da62f4de70fce8250c55f6d1c
janker0718 <690864766@qq.com>
2017-08-15 18:22:48 +0800

    Initial commit

---
+++ src/main/java/cc/janker/opensource/design/partern/adapter/ducks/Duck.java
@@ -0,0 +1,9 @@
+package cc.janker.opensource.design.partern.adapter.ducks;
+
+/**
+ * Created by zhiyongliu3 on 2017/8/15.
+ */
+public interface Duck {
+    public void quack();
+    public void fly();
+}
